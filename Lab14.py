3416db49e3db658755388c165561cec5d61ff6a0
Mark Mariscal <markamariscal@gmail.com>
2018-12-02 14:52:07 -0800

    changes
    finished

--- Lab14.py
+++ Lab14.py
@@ -5,15 +5,26 @@ def Suess():
 
 
   textfiles = text.lower().replace("-"," ").split()
-  
-  dict = dict()
-  
+  uniqueWordCount = 0
+  count = {}
+
   for word in textfiles:
-    if word not in emptylist:
-      dict[word] = 1
-    else:
-      dict[word] += 1
-  return dict
+   if word in count:
+     count[word] += 1
+   else:
+     count[word] = 1
    
-   
+  final = sorted(count.iteritems(), key = lambda x: x[1], reverse = True)
+  
+  print 'The count of how often each of the words appears is:'
+  print  len(count)
+  print '\nPrint out the total distinct count and the count for each of the words:'
+  print final
+  print '\n Tne most commonly occuring word in the book:'
+  return final[0]
+  
+  
+  
+  
+  
   
